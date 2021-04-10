71ae2386cc3ade619fc23c21200dd5c78612a687
codeoncrypto <81856024+codeoncrypto@users.noreply.github.com>
2021-04-10 22:43:02 +0200

    Removed mint function. Assuring fix total supply.

--- contracts/AMZEToken.sol
+++ contracts/AMZEToken.sol
@@ -155,19 +155,6 @@ contract AMZEToken is Context, IAMZE, Ownable {
   }
 
   /**
-   * @dev Creates `amount` tokens and assigns them to `msg.sender`, increasing
-   * the total supply.
-   *
-   * Requirements
-   *
-   * - `msg.sender` must be the token owner
-   */
-  function mint(uint256 amount) public onlyOwner returns (bool) {
-    _mint(_msgSender(), amount);
-    return true;
-  }
-
-  /**
    * @dev Moves tokens `amount` from `sender` to `recipient`.
    *
    * This is internal function is equivalent to {transfer}, and can be used to
@@ -188,23 +175,6 @@ contract AMZEToken is Context, IAMZE, Ownable {
     _balances[sender] = _balances[sender].sub(amount, "AMZE: transfer amount exceeds balance");
     _balances[recipient] = _balances[recipient].add(amount);
     emit Transfer(sender, recipient, amount);
-  }
-
-  /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-   * the total supply.
-   *
-   * Emits a {Transfer} event with `from` set to the zero address.
-   *
-   * Requirements
-   *
-   * - `to` cannot be the zero address.
-   */
-  function _mint(address account, uint256 amount) internal {
-    require(account != address(0), "AMZE: mint to the zero address");
-
-    _totalSupply = _totalSupply.add(amount);
-    _balances[account] = _balances[account].add(amount);
-    emit Transfer(address(0), account, amount);
   }
 
   /**
